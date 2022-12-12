95fb79c43fd3de50e61d6fbc7f0764889f0ef301
matt92101 <matt92101@hotmail.com>
2022-12-12 11:26:38 -0800

    Ready to add titles - switching drivers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'blog#index'
+
+  get 'blogs' => 'blog#index'
+  get 'blogs/:id' => 'blog#show'
+  get 'blogs/new' => 'blog#new'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
